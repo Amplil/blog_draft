d3e555d3276ff4353ac7b467be2f1a930a22b374
amplil <aikotobahaaburakatabura@gmail.com>
2021-07-27 20:24:20 +0900

    mathjaxの更新

--- blog-view.php
+++ blog-view.php
@@ -45,13 +45,16 @@ if (isset($_SESSION['blog_text'])) {
 echo $blog_text;
 ?>
 
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script type="text/javascript"
-        src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
-        MathJax.Hub.Config({
-                TeX: { equationNumbers: { autoNumber: "all" } }
-        });
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" 
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous">
 </script>
+<script>
+    MathJax={
+            tex: {tags:'all'}
+    };</script>
+</script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossorigin="anonymous"></script>
